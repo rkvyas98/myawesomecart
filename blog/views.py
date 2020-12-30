5d0cc710023d9fbc9c319fd6249a90abf09669cb
Rakshit <rkvyas98@gmail.com>
2020-12-30 19:15:46 +0530

    Fixed some Bug

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,14 @@ def index(request):
     return render(request, 'blog/index.html', {'myposts': myposts})
     
 def blogpost(request, id):
-    post = Blogpost.objects.filter(post_id = id)[0]
-    print(post)
-    return render(request, 'blog/blogpost.html', {'post' : post})
+    if id == 1 or id == 2 or id == 3 or id == 4:
+        post = Blogpost.objects.filter(post_id = id)[0]
+        return render(request, 'blog/blogpost.html', {'post' : post})
+    else:
+        return HttpResponse(
+            '''
+            <h2>Sorry You landed the wrong Page!!</h2> <br>
+            <a href="/shop">Go to Home Page</a><br>
+            <a href="/blog">Go to Blog Page</a>
+            '''
+            ) 
